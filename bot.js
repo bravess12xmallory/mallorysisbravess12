92feafc13057179560eb4776b2c8b34818c0771d
bravess12xmallory <42509478+bravess12xmallory@users.noreply.github.com>
2018-08-19 15:44:10 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -182,6 +182,33 @@ Rocket.on('message', message => {//rooms
         message.channel.sendEmbed(embed);
     }
 });
+Rocket.on('message', message => {
+     if(message.content.startsWith(prefix + "clear")) {
+         var args = message.content.split(" ").slice(1);
+ if (!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('انت لا تمتلك الخاصية المطلوبه');
+  if (!args[0]) return message.channel.send('You didn\'t provide any number!!!');
+
+  message.channel.bulkDelete(args[0]).then(() => {
+    const embed = new Discord.RichEmbed()
+      .setColor(0xF16104)
+      .setDescription(`Cleared ${args[0]} messages.`);
+    message.channel.send({ embed });
+
+    const actionlog = message.guild.channels.find('name', 'log');
+
+    if (!actionlog) return message.channel.send('Can\'t find action-log channel. Are you sure that this channel exists and I have permission to view it? **CANNOT POST LOG.**');
+    const embedlog = new Discord.RichEmbed()
+      .setDescription('Brave')
+      .setColor(0xF16104)
+      .addField('Clear By', `<@${message.author.id}> with ID ${message.author.id}`)
+      .addField('Clear in', message.channel)
+      .addField('Time', message.createdAt);
+    actionlog.send(embedlog);
+   
+  });
+};
+
+});
 Rocket.on('message', message => {//help msg
     if (message.author.bot) return;
      if (message.content === prefix + "help") {
