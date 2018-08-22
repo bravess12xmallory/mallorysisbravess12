245ee7035addc0222ff9f45ce508655b82db89f0
bravess12xmallory <42509478+bravess12xmallory@users.noreply.github.com>
2018-08-22 18:29:38 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -678,64 +678,6 @@ client.on('message', message => {
        }
    });
 
-client.on("message", (message) => {
-    
-    if (isCommand(message, "!!ticket")) {
-        const reason = message.content.split(" ").slice(1).join(" ");
-        if (!message.guild.roles.exists("name", "Support.")) return message.channel.send(`This server doesn't have a \`Support.\` role made, so the ticket won't be opened.\nIf you are an administrator, make one with that name exactly and give it to users that should be able to see tickets.`);
-        if (message.guild.channels.exists("name", "tickets" + message.author.id)) return message.channel.send(`**لديك بالفعل تذكرة مفتوحة.**`);
-        message.guild.createChannel(`ticket-${message.author.id}`, "text").then(c => {
-            let role = message.guild.roles.find("name", "Support.");
-            let role2 = message.guild.roles.find("name", "@everyone");
-            c.overwritePermissions(role, {
-                SEND_MESSAGES: true,
-                READ_MESSAGES: true
-            });
-            c.overwritePermissions(role2, {
-                SEND_MESSAGES: false,
-                READ_MESSAGES: false
-            });
-            c.overwritePermissions(message.author, {
-                SEND_MESSAGES: true,
-                READ_MESSAGES: true
-            });
-            message.channel.send(`:white_check_mark: تم إنشاء تذكرتك, #${c.name}.`);
-            const embed = new Discord.RichEmbed()
-                .setColor("#565656")
-                .addField(`Hey ${message.author.username}!`, `
-يرجى محاولة شرح سبب فتح هذه التذكرة بأكبر قدر ممكن من التفاصيل. سيكون لدينا ** فريق الدعم ** قريباً لمساعدتك..`)
-                .setTimestamp();
-            c.send({
-                embed: embed
-            });
-        }).catch(console.error); 
-    }
-
-
-    if (isCommand(message, "close")) {
-        if (!message.channel.name.startsWith(`ticket-`)) return message.channel.send(`**لا يمكنك استخدام هذا الامر خارج روم التذاكر**`);
-
-        message.channel.send(`
-هل أنت واثق؟ بعد التأكيد ، لا يمكنك عكس هذا الإجراء! \ n للتأكيد ، اكتب \ "/ confirm \`. سوف ينتهي المهلة خلال 10 ثوانٍ ويتم إلغاها.`)
-            .then((m) => {
-                message.channel.awaitMessages(response => response.content === '/confirm', {
-                        max: 1,
-                        time: 10000,
-                        errors: ['time'],
-                    })
-                    .then((collected) => {
-                        message.channel.delete();
-                    })
-                    .catch(() => {
-                        m.edit('انتهى إغلاق التذاكر ، لم يتم إغلاق التذكرة.').then(m2 => {
-                            m2.delete();
-                        }, 3000);
-                    });
-            });
-    }
-
-});
-
 //////////////////////////////////////
 
 client.login(process.env.BOT_TOKEN);
